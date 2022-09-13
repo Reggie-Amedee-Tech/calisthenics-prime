87bb9f67ea449b9b62d7f6fddc9b2c999eccb413
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-09-13 11:30:16 -0400

    changed styling of all regimen page to a table structure

--- client/src/components/StartWorkoutRegimen.js
+++ client/src/components/StartWorkoutRegimen.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import InititateWorkoutPage from './InititateWorkoutPage'
 import { Link, useLocation } from 'react-router-dom'
 import axios from 'axios'
 
--- client/src/views/GetAllWorkoutsRegimens.js
+++ client/src/views/GetAllWorkoutsRegimens.js
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { getWorkoutRegimens } from '../features/GetWorkoutRegimens.js';
 import classes from '../assets/getAllWorkoutRegimens.module.css'
-import dumbell from '../pictures/dumbell.png'
-
 
 const GetAllWorkoutsRegimens = () => {
   const workoutRegimen = useSelector(state => state.getWorkoutRegimens)
@@ -15,23 +13,44 @@ const GetAllWorkoutsRegimens = () => {
     dispatch(getWorkoutRegimens())
   }, [])
 
-
   return (
     <div className={classes.Div}>
       <div className={classes.Container}>
-        {workoutRegimen.workoutRegimens.map(workout => {
-          return <div className={classes.WorkoutCard} key={workout._id}>
-            <div>
-              <img src={dumbell} alt="dumbell-icon" className={classes.Img} />
-            </div>
-            <div className={classes.MidCard}>
-            <p>{workout.workoutRegimen}</p>
-            <p>{workout.createdAt}</p>
-            </div>
-            <Link to={`${workout._id}`} className={classes.Link}>View Workout</Link>
-          </div>
+        <table className={classes.Table}>
+          <thead>
+            <tr className={classes.TableRow}>
+              <th className={classes.TableHead}>Regimen Name</th>
+              <th className={classes.TableHead}>Workouts Completed</th>
+              <th className={classes.TableHead}>Date</th>
+              <th className={classes.TableHead}>Time</th>
+              <th className={classes.TableHead}>View</th>
+            </tr>
+          </thead>
+          <tbody>
+            {workoutRegimen.workoutRegimens.map(workout => {
+              let arr = []
+              let date = workout.createdAt.slice(0,10)
+              let year = workout.createdAt.slice(0,4)
 
-        })}
+              for (let i = 0; i < date.length; i++) {
+                if (i > 4) {
+                  arr.push(date[i])
+                  console.log(arr)
+                }
+              }
+
+              const workoutDate = arr.toString().replace(/,/g, "") + "-" + year
+              
+              return <tr key={workout._id} className={classes.TableBottomRow}>
+                <td className={classes.TableData}>{workout.workoutRegimen}</td>
+                <td className={classes.TableData}>{workout.workouts.length}</td>
+                <td className={classes.TableData}>{workoutDate}</td>
+                <td className={classes.TableData}>{workout.completionTime}</td>
+                <td className={classes.TableData}><Link to={`${workout._id}`} className={classes.Link}>View Workout</Link></td>
+              </tr>
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   )
