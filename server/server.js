4572410e087de313a881b975c0d0def6557a786c
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-09-16 18:31:51 -0400

    connecting app to heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 
 require('dotenv').config()
 const PORT = process.env.PORT || 5003
@@ -14,4 +15,14 @@ app.use(express.urlencoded({extended: true}))
 
 require('./routes/exercise.routes')(app)
 require('./routes/exerciseQueue.routes')(app)
+
+
+app.use(express.static(path.resolve(__dirname, "./client/build")))
+app.get("*", function(request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+})
+
 app.listen(PORT, () => console.log('Database successfully connected!'))
+
+
+
