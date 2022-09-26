b8132cea1e4bf99059665e4d87e12eaccff2f577
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-09-25 20:00:30 -0400

    changed procfile and server file

--- server/server.js
+++ server/server.js
@@ -17,9 +17,9 @@ require('./routes/exercise.routes')(app)
 require('./routes/exerciseQueue.routes')(app)
 
 
-app.use(express.static(path.resolve(__dirname, "./client/build")))
+app.use(express.static(path.resolve(__dirname, "../client/build")))
 app.get("*", function(request, response) {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
 })
 
 app.listen(PORT, () => console.log('Database successfully connected!'))
