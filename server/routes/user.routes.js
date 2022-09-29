8f3f5e4d59153c91a5c20f32d2cb4cc26ea6ec9d
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-09-29 12:19:29 -0400

    added user login

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,7 @@
 const UserController = require('../controllers/user.controller')
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 module.exports = function(app) {
     app.post('/register', (request,response) => {
@@ -17,7 +18,32 @@ module.exports = function(app) {
             })
         })
         .catch((e) => {
-            response.status(500).send({message: "password was not successfully hashed", e})
+            response.status(500).send({message: "Password was not successfully hashed", e})
+        })
+    })
+
+    app.post('/login', (request,response) => {
+        User.findOne({email: request.body.email})
+        .then((user) => {
+            bcrypt.compare(request.body.password, user.password)
+            .then((passwordCheck) => {
+                if (!passwordCheck) {
+                    return response.status(400).send({message: "Password does not match!", error})
+                }
+
+                const token = jwt.sign({
+                    userId: user._id,
+                    userEmail: user.email
+                }, "RANDOM_TOKEN", {expiresIn: "24h"})
+                response.status(200).send({message: "Login Successful", email: user.email, token})
+                
+            })
+            .catch((e) => {
+                response.status(500).send({message: "Password does not match!"})
+            })
+        })
+        .catch((e) => {
+            response.status(500).send({message: "Email is invalid", e})
         })
     })
 }
