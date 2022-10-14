4d3b990c5bb38859945508f77e3d4137d7eb34e1
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-10-14 16:01:36 -0400

    made homepage more device responsive

--- server/server.js
+++ server/server.js
@@ -11,19 +11,27 @@ const connectDB = require('./config/mongoose.config')
 connectDB()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 
 require('./routes/exerciseQueue.routes')(app)
 require('./routes/user.routes')(app)
 
 
-app.use(express.static(path.resolve(__dirname, "../client/build")))
-app.get("*", function(request, response) {
-    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
-})
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, "../client/build")))
+    app.get("*", function (request, response) {
+        response.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+    })
+
+}
+
+// app.use(express.static(path.resolve(__dirname, "../client/build")))
+// app.get("*", function(request, response) {
+//     response.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+// })
 
 app.listen(PORT, () => console.log('Database successfully connected!'))
 
