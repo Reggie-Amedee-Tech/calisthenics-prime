67646f67aba880e94ef00456a70d54c82e0d5669
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-10-15 13:52:11 -0400

    took away a log

--- client/src/components/InititateWorkoutPage.js
+++ client/src/components/InititateWorkoutPage.js
@@ -14,7 +14,6 @@ const InititateWorkoutPage = (props) => {
     const [timeSaved, setTimeSaved] = useState(false)
     const location = useLocation()
     const id = location.pathname.slice(1)
-    console.log(id)
 
     const instance = axios.create({
         withCredentials: true
