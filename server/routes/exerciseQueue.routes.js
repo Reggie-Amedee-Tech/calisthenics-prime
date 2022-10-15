00ddbb7e90eb9d8ea6a2c276e7a2297a7f66f2a2
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-10-15 13:56:06 -0400

    added authenticate to update regimen route

--- server/routes/exerciseQueue.routes.js
+++ server/routes/exerciseQueue.routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     app.get('/api/regimen/workoutRegimens', authenticate, ExerciseQueueController.getRegimens)
     app.get('/api/regimen/workoutRegimens/:id', ExerciseQueueController.getOneRegimen)
     app.post('/api/regimen/exerciseQueueCreate', authenticate, ExerciseQueueController.createExersizeQueue)
-    app.put('/api/regimen/:id/exerciseQueueUpdate', ExerciseQueueController.updateRegimens)
+    app.put('/api/regimen/:id/exerciseQueueUpdate', authenticate, ExerciseQueueController.updateRegimens)
     app.delete('/api/regimen/:id/deleteExercise', ExerciseQueueController.deleteWorkout) 
 }
 
